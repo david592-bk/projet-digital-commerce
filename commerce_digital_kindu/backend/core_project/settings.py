d694824721536544a390ed48c4e02220aa6fe2ee
davidbanza <david1999banza@gmail.com>
2026-05-29 18:38:45 +0200

    Backend Dockerfile Changed

--- commerce_digital_kindu/backend/core_project/settings.py
+++ commerce_digital_kindu/backend/core_project/settings.py
@@ -60,7 +60,7 @@ ASGI_APPLICATION = 'core_project.asgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
     }
 else:
     DATABASES = {
